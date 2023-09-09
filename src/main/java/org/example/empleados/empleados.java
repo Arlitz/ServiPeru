6f00eb4b2a4234938484e4882fce830ff9d360b9
JAlonsoSC <alonso_acuario_19@hotmail.com>
2023-09-09 18:17:37 -0500

    proyecto

--- src/main/java/org/example/empleados/empleados.java
+++ src/main/java/org/example/empleados/empleados.java
@@ -2,7 +2,7 @@ package org.example.empleados;
 
 public class empleados {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("TRABAJO DE SERVI PERU");
     }
 
 }
